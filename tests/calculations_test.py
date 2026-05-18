a54176fce9feb9dbd24b29b9fb0e8da174d04deb
seokhaki <seokhaki@gmail.com>
2026-05-18 17:00:37 +0100

    Update calculations_test.py

--- tests/calculations_test.py
+++ tests/calculations_test.py
@@ -59,15 +59,15 @@ def test_get_nth_fibonacci_one():
 
 
 def test_get_nth_fibonacci_ten():
-     """Test with n=10."""
-#     # Arrange
-#     n = 10
+   """Test with n=10."""
+   # Arrange
+   n = 10
 
-#     # Act
-#     result = get_nth_fibonacci(n)
+   # Act
+   result = get_nth_fibonacci(n)
 
-#     # Assert
-#     assert result == 89
+   # Assert
+   assert result == 55
 
 def test_area_of_circle_negative_radius():
    """Test with a negative radius to raise ValueError."""
